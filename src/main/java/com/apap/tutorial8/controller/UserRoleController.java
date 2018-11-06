47605981664e40826e47ab7ca5ce270e2728a49b
selinamrzk <selinamaurizka@gmail.com>
2018-11-06 19:17:13 +0700

    latihan 1 dan 2

---
+++ src/main/java/com/apap/tutorial8/controller/UserRoleController.java
@@ -0,0 +1,59 @@
+package com.apap.tutorial8.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.apap.tutorial8.service.UserRoleService;
+import com.apap.tutorial8.model.PasswordModel;
+import com.apap.tutorial8.model.UserRoleModel;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserRoleController {
+	@Autowired
+	private UserRoleService userService;
+	
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	private String addUserSubmit(@ModelAttribute UserRoleModel user) {
+		userService.addUser(user);
+		return "home";
+	}
+	
+	@RequestMapping("/updatePassword")
+	public String updatePassword() {
+		return "update-password";
+	}
+	
+	@RequestMapping(value="/submitPass", method = RequestMethod.POST)
+	private ModelAndView updatePasswordSubmit(@ModelAttribute PasswordModel pass, Model model, RedirectAttributes redirect) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		UserRoleModel user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+		String msg = "";
+		
+		if(pass.getConfirmPass().equals(pass.getNewPass())) {
+			if(passwordEncoder.matches(pass.getOldPass(), user.getPassword())) {
+				userService.changePassword(user, pass.getNewPass());
+				msg = "Password berhasil diubah!";
+			}
+			else {
+				msg = "Password lama anda salah!";
+			}
+		}
+		else {
+			msg = "Password baru tidak sesuai!";
+		}
+		ModelAndView modelAndView = new ModelAndView("redirect:/user/updatePassword");
+		redirect.addFlashAttribute("msg", msg);
+		return modelAndView;
+	}
+	
+}
